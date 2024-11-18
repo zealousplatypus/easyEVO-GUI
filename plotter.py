565c0cadaa51900397ac1df4ce173125282e3ee1
zealousplatypus <114553141+zealousplatypus@users.noreply.github.com>
2024-11-18 01:00:52 -0800

    Made header lines flexible for different csvs. Uses first line in csv as template header line.

--- plotter.py
+++ plotter.py
@@ -7,24 +7,29 @@ def generate_dfs(file_path):
     with open(file_path, 'r') as file:
         lines = file.readlines()
 
-    # Identify header rows and split data into chunks
-    header = "unixTime,upTime,ambientTemp,mediaTemp,heaterPlateTemp,heaterPWM,growthDuration,growthDurationChange,dilutionDuration,dilutionDurationChange,neutralMediaDispenseCount,cycleDuration,neutralCycleCount,positiveCycleCount,neutralCycleTwoCount,negativeCycleCount,totalCycleCount,fullSpectrumReading,visibleSpectrumReading,infraredReading,OD940\n"
-    header_indices = [i for i, line in enumerate(lines) if line.strip() == header.strip()]
-    
+    # Use the first line as the template for the header
+    header_template = lines[0].strip()
+
+    # Identify header rows based on matching the first line
+    header_indices = [i for i, line in enumerate(lines) if line.strip() == header_template]
+
+    # Extract header names from the first header row
+    header = header_template.split(',')
+
     # Create a list of DataFrames, each representing an experiment
     experiments = []
     for i in range(len(header_indices)):
         start_idx = header_indices[i] + 1
         end_idx = header_indices[i + 1] if i + 1 < len(header_indices) else len(lines)
         experiment_data = ''.join(lines[start_idx:end_idx])
-        experiment_df = pd.read_csv(io.StringIO(experiment_data), header=None, names=header.strip().split(','))
+        experiment_df = pd.read_csv(io.StringIO(experiment_data), header=None, names=header)
         if not experiment_df.empty:
             experiments.append(experiment_df)
     return experiments
 
+
 # plot the ODs over time of the current experiment, with time in hours
 def plot_OD(experiments, ax, experiment_number, start_time_hours=None, end_time_hours=None):
-    print(len(experiments))
     experiment = experiments[experiment_number]
     # Convert upTime from seconds to the specified hours scale
     ax.plot(experiment['upTime'] / 3600.0, experiment['OD940'], label='OD940')
